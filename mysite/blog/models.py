b0efc3644abbb5ac0a2bc0f1b9a55cf08371ea66
Ian Chou <ianchou19@gmail.com>
2020-07-24 18:00:12 -0400

    Blog part ten

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -19,7 +19,7 @@ class Post(models.Model):
     def approve_comments(self):
         return self.comments.filter(approved_comment=True)
 
-    def get_absoulte_url(self):
+    def get_absolute_url(self):
         return reverse("post_detail", kwargs={'pk': self.pk})
 
     def __str__(self):
@@ -31,11 +31,11 @@ class Comment(models.Model):
         'blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_cooment = True
+        self.approved_comment = True
         self.save()
 
     def get_absolute_url(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import render
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
-# Create your views here.
 
 
 class AboutView(TemplateView):
@@ -20,7 +19,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-publish_date')
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
 class PostDetailView(DetailView):
@@ -83,7 +82,9 @@ def add_comment_to_post(request, pk):
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
+    print("test1")
     comment.approve()
+    print("test2")
     return redirect('post_detail', pk=comment.post.pk)
 
 
